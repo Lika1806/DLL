ccae6ff1da45be7a666ef679c6e5d182fe722e46
Angela (Lika) <97836245+Lika1806@users.noreply.github.com>
2023-12-09 22:26:34 +0400

    Update node.py

--- node.py
+++ node.py
@@ -1,18 +1,14 @@
 class Node:
     '''class Node reperesents an element of linked list'''
     def __init__(self, data, prev_node=None, next_node=None):
-        self.__data = data
-        self.__prev = prev_node
-        self.__next = next_node
+        self.data = data
+        self.prev = prev_node
+        self.next = next_node
     
-    def get_data(self):
-        return self.__data
-    def get_prev(self):
-        return self.__prev
     def get_next(self):
         return self.__next
-    def set_data(self,value):
-        self.__data = value
+    def get_prev(self):
+        return self.__prev
     def set_prev(self,value):
         if isinstance(value, Node) or not value:
             self.__prev = value
@@ -24,6 +20,5 @@ class Node:
             return
         else: raise ValueError(f"Node {value} doesn't exist")
  
-    data = property(get_data, set_data)
     prev = property(get_prev, set_prev)
     next = property(get_next, set_next)
